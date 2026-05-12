0239c15f31d6cb47bf4c1cca3527453fa3e1ff53
NicolasBarbosaz <nicolasbarbosacz@gmail.com>
2026-05-12 14:47:33 -0300

    Separei as camadas do projeto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConversorModule } from './conversor/conversor.module';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConversorModule],
+  
 })
 export class AppModule {}
---
+++ src/conversor/conversor.controller.ts
@@ -0,0 +1,15 @@
+import { Controller } from "@nestjs/common";
+
+
+
+
+@Controller('conversor')
+export class ConversorController {
+
+
+
+
+
+
+    
+}
