55d4aba1761a6f9b5e811c5dacf429eb8ab424bc
Pablo <pablodosantos2007@gmail.com>
2026-05-19 22:58:56 -0300

    chore: standardizes prettier configuration and line endings

--- src/conversor/conversor.controller.ts
+++ src/conversor/conversor.controller.ts
@@ -1,15 +1,4 @@
-import { Controller } from "@nestjs/common";
-
-
-
+import { Controller } from '@nestjs/common';
 
 @Controller('conversor')
-export class ConversorController {
-
-
-
-
-
-
-    
-}
+export class ConversorController {}
--- src/conversor/conversor.service.ts
+++ src/conversor/conversor.service.ts
@@ -1,41 +1,33 @@
-import { Injectable } from "@nestjs/common";
-import { Conversor } from "./conversor.interface";
+import { Injectable } from '@nestjs/common';
+import { Conversor } from './conversor.interface';
 
 @Injectable()
 export class ConversorService {
+  private conversoes: Conversor[] = []; // Array para guardar as conversoes
+  private id = 1;
 
-    private conversoes: Conversor[] = [];  // Array para guardar as conversoes
-    private id = 1;
+  create(amount: number, fromCurrency: string) {
+    const dolar = 0.2;
+    const euro = 0.17;
 
-    create(amount: number, fromCurrency: string) {
+    const usdValue = amount * dolar;
+    const eurValue = amount * euro;
 
-        const dolar = 0.20;
-        const euro = 0.17;
+    const newConversao: Conversor = {
+      // Criando o objeto de conversao
+      id: this.id++,
+      amount,
+      fromCurrency,
+      usdValue,
+      eurValue,
+    };
+    this.conversoes.push(newConversao); //aqui eu adicionei a conversao na array com todas as conversoes
+    return newConversao;
+  }
 
-        const usdValue = amount * dolar;
-        const eurValue = amount * euro;
+  findAll(): Conversor[] {
+    //metodo para retornar todas as conversoes
 
-
-        const newConversao: Conversor = {  // Criando o objeto de conversao
-            id: this.id++,
-            amount,
-            fromCurrency,
-            usdValue,
-            eurValue
-        };
-      this.conversoes.push(newConversao); //aqui eu adicionei a conversao na array com todas as conversoes
-        return newConversao;
-    }
-
-    findAll() : Conversor [] {  //metodo para retornar todas as conversoes
-
-       return this.conversoes;         
-    }
-
-
-
+    return this.conversoes;
+  }
 }
-
-  
-  
-
