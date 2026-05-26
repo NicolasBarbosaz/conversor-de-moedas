7b4c4114ceff017bbee8d6b7d12580d9d43be354
Pablo <pablodosantos2007@gmail.com>
2026-05-26 02:55:12 -0300

    chore: adjust indentation and formatting

--- src/conversor/repository/conversor.repository.ts
+++ src/conversor/repository/conversor.repository.ts
@@ -1,15 +1,8 @@
 import { ConversorEntity } from '../entities/conversor.entity';
 
 export abstract class ConversorRepository {
-      
-    abstract save(conversor: ConversorEntity): Promise<ConversorEntity>;
-
-    abstract findAll(): Promise<ConversorEntity[]>;
-
-    abstract findByid(id: number) : Promise<ConversorEntity | null>;
-
-    abstract delete(id: number) : Promise<void>;
-     
-
-
+  abstract save(conversor: ConversorEntity): Promise<ConversorEntity>;
+  abstract findAll(): Promise<ConversorEntity[]>;
+  abstract findByid(id: number): Promise<ConversorEntity | null>;
+  abstract delete(id: number): Promise<void>;
 }
