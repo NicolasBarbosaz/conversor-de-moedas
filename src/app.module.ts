bf0393cf15f1d8017a390865eb9b4dd045626a52
Pablo <pablodosantos2007@gmail.com>
2026-05-26 11:08:27 -0300

    feat: integrate conversion module and setup in-memory repository

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ConversorModule } from './conversor/modules/conversor.modules';
+
 @Module({
   imports: [ConversorModule],
 })
--- src/conversor/modules/conversor.modules.ts
+++ src/conversor/modules/conversor.modules.ts
@@ -1,5 +1,23 @@
 import { ConversorController } from '../controllers/conversions.controller';
 import { Module } from '@nestjs/common';
+import { ConversorRepository } from '../repository/conversor.repository';
+import { InMemoryConversorRepository } from '../repository/conversor-in-memory.repository';
+import { CreateConversorUseCase } from '../use-cases/createConversor';
+import { ListConversorUseCase } from '../use-cases/list-conversor';
+import { UpdateConversorUseCase } from '../use-cases/update-conversor';
+import { DeleteConversorUseCase } from '../use-cases/delete-conversor';
 
-@Module({ controllers: [ConversorController] })
+@Module({
+  controllers: [ConversorController],
+  providers: [
+    {
+      provide: ConversorRepository,
+      useClass: InMemoryConversorRepository,
+    },
+    CreateConversorUseCase,
+    ListConversorUseCase,
+    UpdateConversorUseCase,
+    DeleteConversorUseCase,
+  ],
+})
 export class ConversorModule {}
